480efb12f7d24aff8806252e25a518536d5d14cc
Rookabu <annika-paul1@gmx.de>
2024-08-06 16:47:15 +0200

    wrapping

--- src/Highlight.fs
+++ src/Highlight.fs
@@ -1,4 +1,16 @@
 module Highlighter
 
 open Feliz
-open Fable.
+open Fable.Core.JsInterop 
+
+module HighlighterInterop =
+
+    let highlighter: obj = import "default" "react-highlight-words"
+
+type Highlighter =
+
+    static member inline highlighter props = Interop.reactApi.createElement (HighlighterInterop.highlighter, createObj !!props)
+    static member inline highlightClassName (className: string) = prop.custom ("highlightClassName" , className)
+    static member inline searchWords (target: ResizeArray<string>) = prop.custom ("searchWords" , target)
+    static member inline textToHighlight (txt: string) = prop.custom ("textToHighlight" , txt)
+    
