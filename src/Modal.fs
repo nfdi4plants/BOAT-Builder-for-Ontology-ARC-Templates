ac0be77ef999c29472d144251216d8826402b493
Rookabu <annika-paul1@gmx.de>
2024-08-19 16:58:15 +0200

    wrote modal construct

--- src/Modal.fs
+++ src/Modal.fs
@@ -4,35 +4,33 @@ open Feliz
 open Feliz.Bulma
 
 type DropdownModal = {
-    isModalActive: bool
-    setModalActive: bool -> unit
-
-
+    isActive: bool
+    location: int * int
 }
 
+type ModalContext = {
+    modalState: DropdownModal
+    setter: DropdownModal -> unit 
+}
 type Modal = 
     [<ReactComponent>]
     static member Main() =
 
-        let (isModalActive: bool, setModalActive) = React.useState(false) 
-        
-
-        // isactive und location als state
-        // setter als option definieren
-
-        let setModal (isActive:bool) (location: int * int) =
-            match isModalActive with
-            |true -> 
-            |false ->  
+        let (modalState: DropdownModal, setModalActive) = 
+            React.useState({
+                isActive = false; 
+                location = (0,0) 
+                }
+            )           
          
-        let myDropdownModal = {
-            isModalActive = isModalActive
-            setModalActive = setModalActive 
+        let myDropdownContext = {
+            modalState = modalState
+            setter = setModalActive 
             }
 
-        let modalContext() = React.createContext(name="modal", defaultValue = myDropdownModal)
+        let modalContext() = React.createContext(name="modal", defaultValue = myDropdownContext)
 
-        let (modal:DropdownModal) = React.useContext(modalContext())
+        let (modal:ModalContext) = React.useContext(modalContext())
 
         Html.div []
 
