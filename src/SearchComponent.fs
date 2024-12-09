5deef615451819e026dad248571a21b29ee3f531
Rookabu <annika-paul1@gmx.de>
2024-12-09 17:12:37 +0100

    implementing swate components

---
+++ src/SearchComponent.fs
@@ -0,0 +1,162 @@
+module BuildingBlock.SearchComponent
+
+open Feliz
+open Feliz.DaisyUI
+open Fable.Core.JsInterop
+open Model.BuildingBlock
+open Model
+open Messages
+open ARCtrl
+open Shared
+
+// let private termOrUnitizedSwitch (model:Model) dispatch =
+//     let state = model.AddBuildingBlockState
+//     React.fragment [
+//         Daisy.button.a [
+//             join.item
+//             button.outline
+//             let isActive = state.BodyCellType = CompositeCellDiscriminate.Term
+//             if isActive then button.primary
+//             prop.onClick (fun _ -> BuildingBlock.UpdateBodyCellType CompositeCellDiscriminate.Term |> BuildingBlockMsg |> dispatch)
+//             prop.text "Term"
+//         ]
+//         Daisy.button.a [
+//             join.item
+//             button.outline
+//             let isActive = state.BodyCellType = CompositeCellDiscriminate.Unitized
+//             if isActive then button.primary
+//             prop.onClick (fun _ -> BuildingBlock.UpdateBodyCellType CompositeCellDiscriminate.Unitized |> BuildingBlockMsg |> dispatch)
+//             prop.text "Unit"
+//         ]
+//     ]
+
+open Fable.Core
+
+[<ReactComponent>]
+let private SearchBuildingBlockBodyElement (model: Model, dispatch) =
+    let element = React.useElementRef()
+    Html.div [
+        prop.ref element
+        prop.style [ style.position.relative ]
+        prop.children [
+            Daisy.join [
+                prop.className "w-full"
+                prop.children [
+                    termOrUnitizedSwitch model dispatch
+                    // helper for setting the body cell type
+                    let setter (oaOpt: OntologyAnnotation option) =
+                        let case = oaOpt |> Option.map (fun oa -> !^oa)
+                        BuildingBlock.UpdateBodyArg case |> BuildingBlockMsg |> dispatch
+                    let parent = model.AddBuildingBlockState.TryHeaderOA()
+                    let input = model.AddBuildingBlockState.TryBodyOA()
+                    Components.TermSearch.Input(setter, fullwidth=true, ?input=input, ?parent=parent, displayParent=false, ?portalTermSelectArea=element.current, isjoin=true, classes="border-current")
+                ]
+            ]
+        ]
+    ]
+
+[<ReactComponent>]
+let private SearchBuildingBlockHeaderElement (ui: BuildingBlockUIState, setUi, model: Model, dispatch) =
+    let state = model.AddBuildingBlockState
+    let element = React.useElementRef()
+    Html.div [
+        prop.ref element // The ref must be place here, otherwise the portalled term select area will trigger daisy join syntax
+        prop.style [style.position.relative]
+        prop.children [
+            Daisy.join [
+                prop.className "w-full"
+                prop.children [
+                    // Choose building block type dropdown element
+                    // Dropdown building block type choice
+                    Dropdown.Main(ui, setUi, model, dispatch)
+                    // Term search field
+                    if state.HeaderCellType.HasOA() then
+                        let setter (oaOpt: OntologyAnnotation option) =
+                            let case = oaOpt |> Option.map (fun oa -> !^oa)
+                            BuildingBlock.UpdateHeaderArg case |> BuildingBlockMsg |> dispatch
+                            //selectHeader ui setUi h |> dispatch
+                        let input = model.AddBuildingBlockState.TryHeaderOA()
+                        Components.TermSearch.Input(setter, fullwidth=true, ?input=input, isjoin=true, ?portalTermSelectArea=element.current, classes="border-current")
+                    elif state.HeaderCellType.HasIOType() then
+                        Daisy.input [
+                            prop.readOnly true
+                            prop.valueOrDefault (
+                                state.TryHeaderIO()
+                                |> Option.get
+                                |> _.ToString()
+                            )
+                        ]
+                ]
+            ]
+        ]
+
+    ]
+
+// let private scrollIntoViewRetry (id: string) =
+//     let rec loop (iteration: int) =
+//         let headerelement = Browser.Dom.document.getElementById(id)
+//         if isNull headerelement then
+//             if iteration < 5 then
+//                 Fable.Core.JS.setTimeout (fun _ -> loop (iteration+1)) 100 |> ignore
+//             else
+//                 ()
+//         else
+//             let rect = headerelement.getBoundingClientRect()
+//             if rect.left >= 0 && ((rect.right <= Browser.Dom.window.innerWidth) || (rect.right <= Browser.Dom.document.documentElement.clientWidth)) then
+//                 ()
+//             else
+//                 let config = createEmpty<Browser.Types.ScrollIntoViewOptions>
+//                 config.behavior <- Browser.Types.ScrollBehavior.Smooth
+//                 config.block <- Browser.Types.ScrollAlignment.End
+//                 config.``inline`` <- Browser.Types.ScrollAlignment.End
+//                 //log headerelement
+//                 headerelement.scrollIntoView(config)
+//     loop 0 
+
+let private AddBuildingBlockButton (model: Model) dispatch =
+    let state = model.AddBuildingBlockState
+    Html.div [
+        prop.className "flex justify-center"
+        prop.children [
+            Daisy.button.button  [
+                let header = Helper.createCompositeHeaderFromState state
+                let body = Helper.tryCreateCompositeCellFromState state
+                let isValid = Helper.isValidColumn header
+                button.wide
+                if isValid then
+                    button.success
+                else
+                    button.error
+                    prop.disabled true
+                prop.onClick (fun _ ->
+                    let bodyCells =
+                        if body.IsSome then // create as many body cells as there are rows in the active table
+                            let rowCount = System.Math.Max(1,model.SpreadsheetModel.ActiveTable.RowCount)
+                            Array.init rowCount (fun _ -> body.Value.Copy())
+                        else
+                            Array.empty
+                    let column = CompositeColumn.create(header, bodyCells)
+                    let index = Spreadsheet.Controller.BuildingBlocks.SidebarControllerAux.getNextColumnIndex model.SpreadsheetModel
+                    SpreadsheetInterface.AddAnnotationBlock column |> InterfaceMsg |> dispatch
+                    let id = $"Header_{index}_Main"
+                    scrollIntoViewRetry id
+                )
+                prop.text "Add Column"
+            ]
+        ]
+    ]
+
+[<ReactComponent>]
+let Main (model: Model) dispatch =
+    let state_bb, setState_bb = React.useState(BuildingBlockUIState.init)
+    //let state_searchHeader, setState_searchHeader = React.useState(TermSearchUIState.init)
+    //let state_searchBody, setState_searchBody = React.useState(TermSearchUIState.init)
+    Html.div [
+        prop.className "flex flex-col gap-4"
+        prop.children [
+            SearchBuildingBlockHeaderElement (state_bb, setState_bb, model, dispatch)
+            if model.AddBuildingBlockState.HeaderCellType.IsTermColumn() then
+                SearchBuildingBlockBodyElement (model, dispatch)
+            AddBuildingBlockButton model dispatch
+        ]
+    ]
---
+++ src/SwateBuildingBlock.fs
@@ -0,0 +1,208 @@
+namespace Components
+
+open Feliz
+open Types
+open Browser.Types
+open Fable.SimpleJson
+open Feliz.Bulma
+open Widgets
+
+
+module private InitExtensions =
+
+    type Rect with
+        static member initSizeFromPrefix(prefix: string) =
+            match Size.load prefix with
+            | Some p -> Some p
+            | None -> None
+
+        static member initPositionFromPrefix(prefix: string) =
+            match Position.load prefix with
+            | Some p -> Some p
+            | None -> None
+
+
+open InitExtensions
+open Fable.Core.JsInterop
+
+module private MoveEventListener =
+
+    open Fable.Core.JsInterop
+
+    let ensurePositionInsideWindow (element:IRefValue<HTMLElement option>) (position: Rect) =
+        let maxX = Browser.Dom.window.innerWidth - element.current.Value.offsetWidth;
+        let tempX = position.X
+        let newX = System.Math.Min(System.Math.Max(tempX,0),int maxX)
+        let maxY = Browser.Dom.window.innerHeight - element.current.Value.offsetHeight;
+        let tempY = position.Y
+        let newY = System.Math.Min(System.Math.Max(tempY,0),int maxY)
+        {X = newX; Y = newY}
+
+    let calculatePosition (element:IRefValue<HTMLElement option>) (startPosition: Rect) = fun (e: Event) ->
+        let e : MouseEvent = !!e
+        let tempX = int e.clientX - startPosition.X
+        let tempY = int e.clientY - startPosition.Y
+        let tempPosition = {X = tempX; Y = tempY}
+        ensurePositionInsideWindow element tempPosition
+
+    let onmousemove (element:IRefValue<HTMLElement option>) (startPosition: Rect) setPosition = fun (e: Event) ->
+        let nextPosition = calculatePosition element startPosition e
+        setPosition (Some nextPosition)
+
+    let onmouseup (prefix,element:IRefValue<HTMLElement option>) onmousemove =
+        Browser.Dom.document.removeEventListener("mousemove", onmousemove)
+        if element.current.IsSome then
+            let rect = element.current.Value.getBoundingClientRect()
+            let position = {X = int rect.left; Y = int rect.top}
+            Position.write(prefix,position)
+
+module private ResizeEventListener =
+
+    let onmousemove (startPosition: Rect) (startSize: Rect) setSize = fun (e: Event) ->
+        let e : MouseEvent = !!e
+        let width = int e.clientX - startPosition.X + startSize.X
+        // I did not enable this, as it creates issues with overlays such as the term search dropdown.
+        // The widget card itself has overflow: visible, which makes a set height impossible,
+        // but wihout the visible overflow term search results might require scrolling.
+        // // let height = int e.clientY - startPosition.Y + startSize.Y
+        setSize (Some {X = width; Y = startSize.Y})
+
+    let onmouseup (prefix, element: IRefValue<HTMLElement option>) onmousemove =
+        Browser.Dom.document.removeEventListener("mousemove", onmousemove)
+        if element.current.IsSome then
+            Size.write(prefix,{X = int element.current.Value.offsetWidth; Y = int element.current.Value.offsetHeight})
+
+// module private Elements =
+//     let helpExtendButton (extendToggle: unit -> unit) =
+//         Html.p [
+//             prop.className "w-full text-sm"
+//             prop.children [
+//                 Html.a [
+//                     prop.text "Help";
+//                     prop.style [style.marginLeft length.auto; style.userSelect.none]
+//                     prop.onClick (fun e -> e.preventDefault(); e.stopPropagation(); extendToggle())
+//                 ]
+//             ]
+//         ]
+
+[<RequireQualifiedAccess>]
+type SwateComponent =
+    | _BuildingBlock
+
+    [<ReactComponent>]
+    static member Base(content: ReactElement, prefix: string, rmv: MouseEvent -> unit, annoState: Annotation list, setState: Annotation list -> unit, a: Annotation) =
+        let position, setPosition = React.useState(fun _ -> Rect.initPositionFromPrefix prefix)
+        let size, setSize = React.useState(fun _ -> Rect.initSizeFromPrefix prefix)
+        // let helpIsActive, setHelpIsActive = React.useState(false)
+        let element = React.useElementRef()
+        React.useLayoutEffectOnce(fun _ -> position |> Option.iter (fun position -> MoveEventListener.ensurePositionInsideWindow element position |> Some |> setPosition)) // Reposition widget inside window
+        let resizeElement (content: ReactElement) =
+            Html.div [
+                prop.ref element
+                prop.onMouseDown(fun e ->  // resize
+                    e.preventDefault()
+                    e.stopPropagation()
+                    let startPosition = {X = int e.clientX; Y = int e.clientY}
+                    let startSize = {X = int element.current.Value.offsetWidth; Y = int element.current.Value.offsetHeight}
+                    let onmousemove = ResizeEventListener.onmousemove startPosition startSize setSize
+                    let onmouseup = fun e -> ResizeEventListener.onmouseup (prefix, element) onmousemove
+                    Browser.Dom.document.addEventListener("mousemove", onmousemove)
+                    let config = createEmpty<AddEventListenerOptions>
+                    config.once <- true
+                    Browser.Dom.document.addEventListener("mouseup", onmouseup, config)
+                )
+                prop.className "shadow-md border border-base-300 space-y-4 rounded-lg border-r-2 bg-base-100"
+                prop.style [
+                    style.zIndex 40
+                    style.cursor.eastWestResize//style.cursor.northWestSouthEastResize ;
+                    style.display.flex
+                    // style.paddingRight(2);
+                    style.overflow.visible
+                    style.position.fixedRelativeToWindow
+                    style.minWidth.minContent
+                    if size.IsSome then
+                        style.width size.Value.X
+                    else
+                        style.maxWidth 600
+                        //style.height size.Value.Y
+                    if position.IsNone then
+                        //style.transform.translate (length.perc -50,length.perc -50)
+                        style.top (length.perc 20); style.left (length.perc 20);
+                    else
+                        style.top position.Value.Y; style.left position.Value.X;
+                ]
+                prop.children content
+            ]
+        resizeElement <| Html.div [
+            prop.onMouseDown(fun e -> e.stopPropagation())
+            prop.className "border-b border-black cursor-default flex flex-col grow"
+            prop.children [
+                Html.div [
+                    prop.onMouseDown(fun e -> // move
+                        e.preventDefault()
+                        e.stopPropagation()
+                        let x = e.clientX - element.current.Value.offsetLeft
+                        let y = e.clientY - element.current.Value.offsetTop;
+                        let startPosition = {X = int x; Y = int y}
+                        let onmousemove = MoveEventListener.onmousemove element startPosition setPosition
+                        let onmouseup = fun e -> MoveEventListener.onmouseup (prefix, element) onmousemove
+                        Browser.Dom.document.addEventListener("mousemove", onmousemove)
+                        let config = createEmpty<AddEventListenerOptions>
+                        config.once <- true
+                        Browser.Dom.document.addEventListener("mouseup", onmouseup, config)
+                    )
+                    prop.className "cursor-move flex justify-end bg-gradient-to-br from-primary to-base-200 rounded-lg"
+                    prop.children [
+                        // Components.Components.DeleteButton(props=[prop.className "btn-ghost glass";prop.onClick (fun e -> e.stopPropagation(); rmv e)])
+                        Html.span [
+                            prop.className "delete float-right mt-0"
+                            prop.onClick (fun _ -> 
+                                let newAnnoList: Annotation list = annoState |> List.filter (fun x -> x = a |> not)  
+                                // List.removeAt (List.filter (fun x -> x = a) state) state
+                                setState newAnnoList
+                            )
+                        ]
+                    ]
+                ]
+                Html.div [
+                    prop.className "p-2"
+                    prop.children [
+                        content
+                    ]
+                ]
+                // if help.IsSome then
+                //     Html.div [
+                //         prop.tabIndex 0
+                //         prop.className "text-primary collapse bg-opacity-50 rounded-none max-w-none
+                //         focus:bg-primary focus:text-primary-content
+                //         prose prose-a:text-primary-content
+                //         prose-code:text-base-content prose-code:bg-base-200 prose-code:rounded-none"
+                //         prop.children [
+                //             Bulma [
+                //                 prop.className "px-2 py-1 min-h-0 flex"
+                //                 prop.children [
+                //                     Html.div [prop.text "Help"; prop.className "text-sm font-light ml-auto"]
+                //                 ]
+                //             ]
+                //             Daisy.collapseContent [
+                //                 prop.className "marker:text-primary-content"
+                //                 prop.children [
+                //                     help.Value
+                //                 ]
+                //             ]
+                //         ]
+                //     ]
+            ]
+        ]
+
+    static member BuildingBlock (model, dispatch, rmv: MouseEvent -> unit, annoState, setState, a) =
+        let content = BuildingBlock.SearchComponent.Main model dispatch
+        // let help = React.fragment [
+        //     Html.p "Add a new Building Block."
+        //     Html.ul [
+        //         Html.li "If a cell is selected, a new Building Block is added to the right of the selected cell."
+        //         Html.li "If no cell is selected, a new Building Block is appended at the right end of the table."
+        //     ]
+        // ]
+        let prefix = WidgetLiterals.BuildingBlock
+        SwateComponent.Base(content, prefix, rmv, annoState, setState, a)
